ebc5df8846fc0296acb5c76f834331eb7ab62e90
mncc8337 <mncc8327@gmail.com>
2026-05-08 16:53:53 +0700

    spinwheel weights

--- cog/spin.py
+++ cog/spin.py
@@ -2,6 +2,7 @@ from discord import Interaction
 from discord import app_commands
 from discord.ext.commands import GroupCog
 from lib.lucky_wheel import LuckyWheel
+from typing import Optional
 
 
 class SpinCog(GroupCog, group_name="spin"):
@@ -15,8 +16,14 @@ class SpinCog(GroupCog, group_name="spin"):
         self.wheel.save()
 
     @app_commands.command(name="add", description="add an item to lucky wheel")
-    async def dd(self, interaction: Interaction, name: str, value: int):
-        self.wheel.add(name, value, interaction)
+    async def dd(
+        self,
+        interaction: Interaction,
+        name: str,
+        value: int,
+        weight: Optional[int] = 1
+    ):
+        self.wheel.add(name, value, weight, interaction)
         await interaction.response.send_message(f"item {name} added")
 
         self.wheel.save()
@@ -27,6 +34,37 @@ class SpinCog(GroupCog, group_name="spin"):
             await interaction.response.send_message("no such item " + name)
         self.wheel.remove(name, interaction)
         await interaction.response.send_message(f"item \"{name}\" removed")
+
+        self.wheel.save()
+
+    @app_commands.command(name="set_weight", description="set new weight for an item")
+    async def set_weight(
+        self,
+        interaction: Interaction,
+        name: str,
+        weight: int
+    ):
+        if weight <= 0:
+            await interaction.response.send_message(
+                content="invalid weight, must be a positive integer",
+                ephemeral=True,
+            )
+            return
+
+        self.wheel.set_weight(name, weight, interaction)
+        await interaction.response.send_message(f"item {name}'s weight set to {weight}")
+
+        self.wheel.save()
+
+    @app_commands.command(name="set_value", description="set new value for an item")
+    async def set_value(
+        self,
+        interaction: Interaction,
+        name: str,
+        value: int
+    ):
+        self.wheel.set_value(name, value, interaction)
+        await interaction.response.send_message(f"item {name}'s value set to {value}")
 
         self.wheel.save()
 
--- lib/lucky_wheel.py
+++ lib/lucky_wheel.py
@@ -1,4 +1,4 @@
-from discord import Interaction
+from discord import Interaction, Embed
 import random
 
 import lib.data_loader as data_loader
@@ -17,36 +17,73 @@ class LuckyWheel(data_loader.Data):
         item = random.choice(list(local_data["item"].keys()))
 
         usr_id_str = str(interaction.user.id)
+        value = local_data["item"][item][0]
 
         if str(interaction.user.id) in local_data["user"].keys():
-            local_data["user"][usr_id_str]["point"] += local_data["item"][item]
+            local_data["user"][usr_id_str]["point"] += value
         else:
             local_data["user"][usr_id_str] = {
                 "name": interaction.user.name,
-                "point": local_data["item"][item],
+                "point": value,
             }
 
-        await interaction.response.send_message(f"{interaction.user.mention} got {item}, value {local_data["item"][item]} points")
+        await interaction.response.send_message(f"{interaction.user.mention} got {item}, value {value} points")
 
-    def add(self, key: str, val: int, interaction: Interaction):
+    def add(self, key: str, val: int, weight: int, interaction: Interaction):
         local_data = self.get_data(interaction)
-        local_data["item"][key] = val
+        local_data["item"][key] = [val, weight]
 
     def remove(self, key: str, interaction: Interaction):
         local_data = self.get_data(interaction)
         local_data["item"].pop(key)
 
+    def set_weight(self, key: str, weight: int, interaction: Interaction):
+        local_data = self.get_data(interaction)
+        local_data["item"][key][1] = weight
+
+    def set_value(self, key: str, value: int, interaction: Interaction):
+        local_data = self.get_data(interaction)
+        local_data["item"][key][0] = value
+
     async def list(self, interaction: Interaction):
         local_data = self.get_data(interaction)
-        msg = ""
+
+        total_weight = 0
+        expected_value = 0
+
+        items = []
 
         for item in local_data["item"].keys():
-            msg += f"{item}: {local_data["item"][item]}\n"
+            item_dat = local_data["item"][item]
+            total_weight += item_dat[1]
+            expected_value += item_dat[0] * item_dat[1]
+            items.append([item_dat[0], item_dat[1], item])
+        if len(local_data["item"]) == 0:
+            total_weight = 1
 
-        if msg != "":
-            await interaction.response.send_message(msg)
-        else:
+        items.sort(key=lambda x: (x[0], x[1]), reverse=True)
+
+        expected_value /= total_weight
+
+        name_field = ""
+        value_field = ""
+        weight_field = ""
+
+        for item in items:
+            name_field += item[2] + '\n'
+            value_field += str(item[0]) + '\n'
+            weight_field += str(item[1]) + f" ({item[1] * 100 / total_weight:.2f}%)\n"
+
+        embed = Embed(title="Wheel Items", color=0x32a852)
+        embed.add_field(name="Item", value=name_field, inline=True)
+        embed.add_field(name="Value", value=value_field, inline=True)
+        embed.add_field(name="Weight", value=weight_field, inline=True)
+        embed.add_field(name="Expected value", value=f"{expected_value:.2f}")
+
+        if name_field == "":
             await interaction.response.send_message("no item")
+        else:
+            await interaction.response.send_message(embed=embed)
 
     async def user(self, interaction: Interaction):
         local_data = self.get_data(interaction)
