bcd044b46f9aa9bf00c8e8b8299475a722998955
mncc8337 <mncc8327@gmail.com>
2026-05-08 17:23:43 +0700

    load matplotlib before pillow to prevent termux segfault (idk why but that fixed the issue)

--- cog/__init__.py
+++ cog/__init__.py
@@ -2,14 +2,14 @@ from cog.random import RandomCog
 from cog.todo import TodoCog
 from cog.spin import SpinCog
 from cog.ai import AiCog
-from cog.image import ImageCog
 from cog.weather import WeatherCog
+from cog.image import ImageCog
 
 __all__ = [
     "RandomCog",
     "TodoCog",
     "SpinCog",
     "AiCog",
-    "ImageCog",
     "WeatherCog",
+    "ImageCog",
 ]
